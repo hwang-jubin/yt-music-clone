f643934d91371251bafb6dd90e9a13187964d390
황주빈 <hjb06288@gmail.com>
2024-07-31 14:23:13 +0900

    feat: page,param,searchParam

---
+++ app/(site)/page.tsx
@@ -0,0 +1,14 @@
+import { sleep } from '@/lib/utils'
+import React from 'react'
+
+const page = async () => {
+
+
+    return (
+        <div>
+            HomePage
+        </div>
+    )
+}
+
+export default page
